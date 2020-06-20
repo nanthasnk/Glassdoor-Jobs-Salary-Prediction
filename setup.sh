d420799e92397ea7f2196e5086a9377c94f90da3
Nantha Kumar S <nanthakumarsnk@gmail.com>
2020-06-20 12:42:28 +0530

    Deployemnt #setup1

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS = false\n\
-port = $PORT\n\
-\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config .toml
